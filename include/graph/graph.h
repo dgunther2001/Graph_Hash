52d3bbf4649ee8ea431275c82170c3c236bc5967
dgunther2001 <danielgunther2001@gmail.com>
2024-04-24 20:51:58 -0400

    Generic deletion and edge insertion

--- include/graph/graph.h
+++ include/graph/graph.h
@@ -2,26 +2,236 @@
 #include "../external_libs/Linked_List/include/linked_list/linked_list.h"
 #include <iostream>
 #include <string>
+#include <iomanip> 
+
+/*
+template <typename Object> 
+class List_Entries {
+private:
+    typename std::conditional< 
+    std::is_same<Object, int>::value || 
+    std::is_same<Object, char>::value || 
+    std::is_same<Object, float>::value || 
+    std::is_same<Object, double>::value || 
+    std::is_same<Object, bool>::value || 
+    std::is_same<Object, short>::value || 
+    std::is_same<Object, long>::value || 
+    std::is_same<Object, long long>::value || 
+    std::is_same<Object, unsigned short>::value || 
+    std::is_same<Object, unsigned int>::value || 
+    std::is_same<Object, unsigned long>::value || 
+    std::is_same<Object, unsigned long long>::value, 
+    Object, 
+    Object*
+    >::type object;
+
+    std::string key;
+
+public:
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    List_Entries(std::string k, Object* obj) {
+        key = k;
+        object = obj;
+    }
+
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    List_Entries(std::string k, Object obj) {
+        key = k;
+        object = obj;
+    }
+
+    bool operator==(const List_Entries& otherEntry) const {
+        return (key == otherEntry.key && object == otherEntry.object);
+    }
+    bool operator!=(const List_Entries& otherEntry) const {
+        return (key != otherEntry.key || object != otherEntry.object);
+    }
+
+    std::string getKey() {
+        return key;
+    }
+
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    Object* getObject() {
+        return object;
+    }
+
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    Object getObject() {
+        return object;
+    }
+
+};
+*/
 
 template <typename Object> 
 class Graph_Node {
 private:
+    typename std::conditional< 
+    std::is_same<Object, int>::value || 
+    std::is_same<Object, char>::value || 
+    std::is_same<Object, float>::value || 
+    std::is_same<Object, double>::value || 
+    std::is_same<Object, bool>::value || 
+    std::is_same<Object, short>::value || 
+    std::is_same<Object, long>::value || 
+    std::is_same<Object, long long>::value || 
+    std::is_same<Object, unsigned short>::value || 
+    std::is_same<Object, unsigned int>::value || 
+    std::is_same<Object, unsigned long>::value || 
+    std::is_same<Object, unsigned long long>::value, 
+    Object, 
+    Object*
+    >::type object;
+
     std::string key; // the key which should be contained within the object
-    Object object; // the object itself
-    Linked_List<Graph_Node*> adjList; // linked list of graph_node pointers
 
 public:
+
+    std::string getKey() {
+        return key;
+    }
+    
+    Linked_List<Graph_Node<Object>*> adjList;
+
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    Graph_Node(std::string k, Object* obj) {
+        key = k;
+        object = obj;
+    }
+
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
     Graph_Node(std::string k, Object obj) {
         key = k;
         object = obj;
     }
+
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    Object* getObject() {
+        return object;
+    }
+
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    Object getObject() {
+        return object;
+    }
+
+    
 };
 
 template <typename Object>
 class Graph {
+    /*
+    typename std::conditional< 
+    std::is_same<Object, int>::value || 
+    std::is_same<Object, char>::value || 
+    std::is_same<Object, float>::value || 
+    std::is_same<Object, double>::value || 
+    std::is_same<Object, bool>::value || 
+    std::is_same<Object, short>::value || 
+    std::is_same<Object, long>::value || 
+    std::is_same<Object, long long>::value || 
+    std::is_same<Object, unsigned short>::value || 
+    std::is_same<Object, unsigned int>::value || 
+    std::is_same<Object, unsigned long>::value || 
+    std::is_same<Object, unsigned long long>::value, 
+    Hash_Table<Graph_Node<Object>>, 
+    Hash_Table<Graph_Node<Object*>>
+    >::type myMap;
+    */
+
     Hash_Table<Graph_Node<Object>> myMap;
     int size;
     bool digraph;
+
 public:
     /* CONSTRUCTORS */
     Graph(int hashSize, hashfunction* hf, bool d) : myMap(hf, hashSize) {
@@ -68,24 +278,197 @@ public:
         }
     }
 
+    /*
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    Graph_Node<Object*> getGraphNode(std::string key) {
+        //if (!graphContainsNode(key)) { return NULL; }
+        Node<Graph_Node<Object>>* returnObj = *(myMap.hash_get_entry(key));
+        return returnObj;
+    }
+    */
 
+   /*
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    */
+    Graph_Node<Object>* getGraphNode(std::string key) {
+        //if (!graphContainsNode(key)) { return NULL; }
+        Node<Graph_Node<Object>>* returnObj = (myMap.hash_get_entry(key));
+        return returnObj->getObject();//.getObject();
+    }
+
+    
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    void insertNode(std::string key, Object* value) {
+        if (graphContainsNode(key)) {
+            return;
+        } else {
+            Graph_Node<Object>* newNode = new Graph_Node<Object>(key, value);
+            myMap.hash_insert(key, newNode);
+        }
+    }
+
+
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
     void insertNode(std::string key, Object value) {
         if (graphContainsNode(key)) {
             return;
         } else {
-            Graph_Node<Object>* newNode = new Graph_Node(key, value);
-            myMap.hash_insert(key, *newNode);
+            Graph_Node<Object>* newNode = new Graph_Node<Object>(key, value);
+            myMap.hash_insert(key, newNode);
         }
     }
 
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    void insertEdge(std::string keyFrom, std::string keyTo) {
+        if (!graphContainsNode(keyFrom) || !graphContainsNode(keyTo)) { // if one of the keys doesn't exist, we cannot create a connection
+            return;
+        } else {
+            Graph_Node<Object>* to = myMap.hash_get_entry(keyTo)->getObject();
+            Graph_Node<Object>* from = myMap.hash_get_entry(keyFrom)->getObject();
+
+            //append the to node to the from node
+
+            to->adjList.addNode(&from);
+    
+            if (!digraph) {
+                from->adjList.addNode(&to);
+            } 
+            
+        }
+    }
+
+    // TODO IMPLEMENT THE PRIMITIVE VERSION OF ADD EDGE!!!!!!
+    /*
+    template <typename T = Object, typename std::enable_if<std::is_same<T, int>::value || 
+    std::is_same<T, char>::value || 
+    std::is_same<T, float>::value || 
+    std::is_same<T, double>::value || 
+    std::is_same<T, bool>::value || 
+    std::is_same<T, short>::value || 
+    std::is_same<T, long>::value || 
+    std::is_same<T, long long>::value || 
+    std::is_same<T, unsigned short>::value || 
+    std::is_same<T, unsigned int>::value || 
+    std::is_same<T, unsigned long>::value || 
+    std::is_same<T, unsigned long long>::value>::type* = nullptr>
+    void insertEdge(std::string keyFrom, std::string keyTo) {
+        if (!graphContainsNode(keyFrom) || !graphContainsNode(keyTo)) { // if one of the keys doesn't exist, we cannot create a connection
+            return;
+        } else {
+            Graph_Node<Object>* to = myMap.hash_get_entry(keyTo)->getObject();
+            Graph_Node<Object>* from = myMap.hash_get_entry(keyFrom)->getObject();
+
+            //append the to node to the from node
+
+            to->adjList.addNode(&from);
+    
+            if (!digraph) {
+                from->adjList.addNode(&to);
+            } 
+            
+        }
+    }
+    */
+    
+    
+    
+    template <typename T = Object, typename std::enable_if<!std::is_same<T, int>::value && 
+    !std::is_same<T, char>::value && 
+    !std::is_same<T, float>::value && 
+    !std::is_same<T, double>::value && 
+    !std::is_same<T, bool>::value && 
+    !std::is_same<T, short>::value && 
+    !std::is_same<T, long>::value && 
+    !std::is_same<T, long long>::value && 
+    !std::is_same<T, unsigned short>::value && 
+    !std::is_same<T, unsigned int>::value && 
+    !std::is_same<T, unsigned long>::value && 
+    !std::is_same<T, unsigned long long>::value>::type* = nullptr>
     void printGraph() {
         for(int i = 0; i < myMap.getSize(); i++) {
             if (myMap.entries[i] != NULL) {
-                std::cout << myMap.entries[i]->getKey() << "\n";
+                std::cout << std::setw(1) << myMap.entries[i]->getKey() << "\t";
+                std::cout << std::setw(2) << "(" << myMap.entries[i]->getObject() << ")";
+                std::cout << std::setw(5) << ":" << std::setw(5);
+                std::cout << "{";
+                for(int j = 0; j < myMap.entries[i]->getObject()->adjList.getSize(); j++) {
+                    auto objPtr = myMap.entries[i]->getObject()->adjList.getObjAtIndex(j);
+                    if (objPtr != nullptr) {
+                        std::cout << objPtr->getKey() << ", ";
+                    }
+                    /*
+                    Object obj = currentGraphNode->getObject();
+                    
+
+                    if (j != myMap.entries[i]->getObject()->adjList.getSize() - 1) {
+                        std::cout << ", ";
+                    }
+                    */
+                }
+                std::cout << "}";
+                std::cout << "\n";
             }
+
+
         }
     }
 
+    
     /* SIMPLE TEST FUNCTIONS..
 
     void mapInsert(std::string key, Object obj) {
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
 #include "graph/graph.h"
 #include <hash_table/hash_table.h>
+using namespace std;
+
+class students {
+private: 
+    std::string name;
+    int age;
+
+public:
+
+    string getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    students(std::string n, int a) {
+        name = n;
+        age = a;
+    }
+    
+    
+    bool operator==(const students& otherStudent) const {
+        return (name == otherStudent.name && age == otherStudent.age);
+    }
+    
+    
+
+    bool operator!=(const students& otherStudent) const {
+        return (name != otherStudent.name || age != otherStudent.age);
+    }
+
+    
+    friend ostream& operator<< (ostream& os, const students& s)
+    {
+        os << s.name << ": " << s.age;
+        return os;
+    }
+    
+
+};
 
 int main() {
+
+    Graph<students> myGraph1(10, false);
+    
+    students* student1 = new students("Daniel", 23);
+    students* student2 = new students("Hannah", 22);
+    students* student3 = new students("Zach", 23);
+
+    //cout << student3 << "\n";
+
+    myGraph1.insertNode(student1->getName(), student1);
+    myGraph1.insertNode(student2->getName(), student2);
+    myGraph1.insertNode(student3->getName(), student3);
+
+    myGraph1.insertEdge("Daniel", "Hannah");
+
+    myGraph1.printGraph();
+
     //std::cout << "Hello Graphs!\n";
     //Hash_Table<int> myTable(10);
 
@@ -10,11 +69,27 @@ int main() {
 
     //myTable.printTable();
 
-    Graph<int> myGraph(5);
+    /*
+    Graph<int> myGraph(10);
 
     myGraph.insertNode("Daniel", 77);
+    myGraph.insertNode("Hannah", 65);
+
+    Graph_Node<int>* myNode = new Graph_Node("Joe", 27);
+
+    std::cout << myNode->getKey() << "\n";
+    std::cout << myNode->getObject() << "\n";
+
+
+
+    std::cout << myGraph.getGraphNode("Daniel")->getKey() << "\n";
+    std::cout << myGraph.getGraphNode("Daniel")->getObject() << "\n";
+    
+
+    myGraph.insertEdge("Daniel", "Hannah");
+    */
     //myGraph.mapInsert("Daniel", 23);
-    myGraph.printGraph();
+    //myGraph.printGraph();
 
     return 0;
 }
